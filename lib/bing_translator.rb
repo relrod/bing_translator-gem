e902b5917a3db50fe12f8d6e5ecc5c54eeeb920b
Kang HJ <kanghongjin@gmail.com>
2015-10-28 12:14:09 +0800

    add function for translate_array2

--- lib/bing_translator.rb
+++ lib/bing_translator.rb
@@ -60,6 +60,22 @@ class BingTranslator
     array_wrap(result(:translate_array, params)[:translate_array_response]).map{|r| r[:translated_text]}
   end
 
+ def translate_array2(texts, params = {})
+    raise "Must provide :to." if params[:to].nil?
+
+    # Important notice: param order makes sense in SOAP. Do not reorder or delete!
+    params = {
+      'texts'       => { 'arr:string' => texts },
+      'from'        => params[:from].to_s,
+      'to'          => params[:to].to_s,
+      'category'    => 'general',
+      'contentType' => params[:content_type] || 'text/plain'
+    }
+
+    array_wrap(result(:translate_array2, params)[:translate_array2_response]).map{|r| [r[:translated_text], r[:alignment]]}
+  end
+
+
   def detect(text)
     params = {
       'text'     => text.to_s,
