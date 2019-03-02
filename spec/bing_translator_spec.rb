3069c8a551483f9db6da0cb9d4ea1687db12af09
Kamil Bednarz <kamil.bednarz@u2i.com>
2019-03-02 22:21:32 +0100

    Reorganize specs

--- spec/bing_translator_spec.rb
+++ spec/bing_translator_spec.rb
@@ -7,30 +7,14 @@ describe BingTranslator do
     File.read(File.join(File.dirname(__FILE__), 'etc', filename))
   end
 
-  let(:message_en) do
-    'This message should be translated'
-  end
+  let(:api_key) { ENV.fetch('COGNITIVE_SUBSCRIPTION_KEY') }
+  let(:message_en) { 'This message should be translated' }
+  let(:message_en_other) { 'This message should be too translated' }
+  let(:long_text) { load_file('long_text') }
+  let(:long_unicode_text) { load_file('long_unicode_text.txt') }
+  let(:long_html_text) { load_file('long_text.html') }
 
-  let(:message_en_other) do
-    'This message should be too translated'
-  end
-
-  let(:long_text) do
-    load_file('long_text')
-  end
-
-  let(:long_unicode_text) do
-    load_file('long_unicode_text.txt')
-  end
-
-  let(:long_html_text) do
-    load_file('long_text.html')
-  end
-
-  let(:translator) do
-    BingTranslator.new(ENV.fetch('COGNITIVE_SUBSCRIPTION_KEY'),
-                       skip_ssl_verify: false)
-  end
+  let(:translator) { described_class.new(api_key, skip_ssl_verify: false) }
 
   describe '#translate' do
     it 'translates text' do
@@ -69,19 +53,29 @@ describe BingTranslator do
       result = translator.translate 'Diese Meldung sollte übersetzt werden', to: :en
       expect(result).to eq message_en
     end
+
+    context 'when invalid language is specified' do
+      it 'throws a reasonable error' do
+        expect { translator.translate 'hola', from: :invlaid, to: :en }.to raise_error(BingTranslator::Exception)
+      end
+    end
   end
 
-  it 'translates array of texts' do
-    result = translator.translate_array [message_en, message_en_other], from: :en, to: :fr
-    expect(result).to eq ['Ce message devrait être traduit', 'Ce message devrait être aussi traduit']
+  describe '#translate_array' do
+    it 'translates array of texts' do
+      result = translator.translate_array [message_en, message_en_other], from: :en, to: :fr
+      expect(result).to eq ['Ce message devrait être traduit', 'Ce message devrait être aussi traduit']
+    end
   end
 
-  it 'translates array of texts, with word alignment information' do
-    result = translator.translate_array2 [message_en, message_en_other], from: :en, to: :de
-    expect(result).to eq [['Diese Meldung sollte übersetzt werden',
-                           '0:3-0:4 5:11-6:12 13:18-14:19 20:21-31:36 23:32-21:29'],
-                          ['Diese Meldung sollte auch übersetzt werden',
-                           '0:3-0:4 5:11-6:12 13:18-14:19 20:21-36:41 23:25-21:24 27:36-26:34']]
+  describe '#translate_array2' do
+    it 'translates array of texts, with word alignment information' do
+      result = translator.translate_array2 [message_en, message_en_other], from: :en, to: :de
+      expect(result).to eq [['Diese Meldung sollte übersetzt werden',
+                             '0:3-0:4 5:11-6:12 13:18-14:19 20:21-31:36 23:32-21:29'],
+                            ['Diese Meldung sollte auch übersetzt werden',
+                             '0:3-0:4 5:11-6:12 13:18-14:19 20:21-36:41 23:25-21:24 27:36-26:34']]
+    end
   end
 
   describe '#detect' do
@@ -100,20 +94,25 @@ describe BingTranslator do
     end
   end
 
-  it 'throws a reasonable error when the Bing translate API returns an error' do
-    expect { translator.translate 'hola', from: :invlaid, to: :en }.to raise_error(BingTranslator::Exception)
+  describe 'supported_language_codes' do
+    it 'lists supported language codes' do
+      result = translator.supported_language_codes
+      expect(result).to include('en')
+      expect(result).to include('es')
+      expect(result).to include('de')
+    end
   end
 
-  it 'is able to list languages that the API supports' do
-    result = translator.supported_language_codes
-    expect(result).to include('en')
-    expect(result).to include('es')
-    expect(result).to include('de')
-  end
+  describe '#language_names' do
+    it 'converts language codes to full names' do
+      expect(translator.language_names(%w[en es de])).to eq(%w[English Spanish German])
+    end
 
-  it 'is able to get language names from language codes' do
-    expect(translator.language_names(%w[en es de])).to eq %w[English Spanish German]
-    expect(translator.language_names(%w[en es de], 'de')).to eq %w[Englisch Spanisch Deutsch]
+    context 'when second argument with target language is used' do
+      it 'converts the codes to full names in requested language'do
+        expect(translator.language_names(%w[en es de], 'de')).to eq(%w[Englisch Spanisch Deutsch])
+      end
+    end
   end
 
   context 'when credentials are invalid' do
