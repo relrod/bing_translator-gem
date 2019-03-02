fffa3e2d7a72095d44f9342c664e7f2c01a9f0ca
Kamil Bednarz <kamil.bednarz@u2i.com>
2019-03-03 00:37:49 +0100

    Code refactoring: extract authorization to a nested class

--- lib/bing_translator.rb
+++ lib/bing_translator.rb
@@ -10,14 +10,94 @@ require 'net/https'
 require 'json'
 
 class BingTranslator
-  API_HOST = 'https://api.cognitive.microsofttranslator.com'.freeze
-  COGNITIVE_ACCESS_TOKEN_URI = URI.parse('https://api.cognitive.microsoft.com/sts/v1.0/issueToken').freeze
-
   class Exception < StandardError; end
 
+  class ApiClient
+    API_HOST = 'https://api.cognitive.microsofttranslator.com'.freeze
+    COGNITIVE_ACCESS_TOKEN_URI =
+      URI.parse('https://api.cognitive.microsoft.com/sts/v1.0/issueToken').freeze
+
+    def initialize(subscription_key, skip_ssl_verify)
+      @subscription_key = subscription_key
+      @skip_ssl_verify = skip_ssl_verify
+    end
+
+    def get(path, params: {}, headers: {}, authorization: false)
+      uri = request_uri(path, params)
+      request = Net::HTTP::Get.new(uri.request_uri, default_headers(authorization).merge(headers))
+
+      json_response(uri, request)
+    end
+
+    def post(path, params: {}, headers: {}, data: {}, authorization: true)
+      uri = request_uri(path, params)
+
+      request = Net::HTTP::Post.new(uri.request_uri, default_headers(authorization).merge(headers))
+      request.body = data
+
+      json_response(uri, request)
+    end
+
+    private
+
+    def default_headers(authorization = true)
+      headers = { 'Content-Type' => 'application/json' }
+      headers['Authorization'] = "Bearer #{access_token}" if authorization
+      headers
+    end
+
+    def json_response(uri, request)
+      http = http_client(uri)
+
+      response = http.request(request)
+
+      raise Exception.new("Unsuccessful API call: Code: #{response.code}") unless response.code == '200'
+      JSON.parse(response.body)
+    end
+
+    def http_client(uri)
+      http = Net::HTTP.new(uri.host, 443)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE if @skip_ssl_verify
+      http
+    end
+
+    def request_uri(path, params)
+      encoded_params = URI.encode_www_form(params.merge('api-version' => '3.0'))
+      uri = URI.parse("#{API_HOST}#{path}")
+      uri.query = encoded_params
+      uri
+    end
+
+    def access_token
+      if @access_token.nil? || Time.now < @access_token_expiration_time
+        @access_token = request_new_access_token
+      end
+      @access_token
+    end
+
+    def request_new_access_token
+      headers = {
+        'Ocp-Apim-Subscription-Key' => @subscription_key
+      }
+
+      http = Net::HTTP.new(COGNITIVE_ACCESS_TOKEN_URI.host, COGNITIVE_ACCESS_TOKEN_URI.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE if @skip_ssl_verify
+
+      response = http.post(COGNITIVE_ACCESS_TOKEN_URI.path, '', headers)
+      if response.code != '200'
+        raise Exception.new('Invalid credentials')
+      else
+        @access_token_expiration_time = Time.now + 480
+        response.body
+      end
+    end
+  end
+
   def initialize(subscription_key, options = {})
-    @skip_ssl_verify = options.fetch(:skip_ssl_verify, false)
-    @subscription_key = subscription_key
+    skip_ssl_verify = options.fetch(:skip_ssl_verify, false)
+    @api_client = ApiClient.new(subscription_key, skip_ssl_verify)
   end
 
   def translate(text, from: nil, to:, html: false)
@@ -27,22 +107,21 @@ class BingTranslator
     params[:from] = from if from
     data = [{ 'Text' => text }].to_json
 
-    response_json = api_call('/translate', params, data)
+    response_json = api_client.post('/translate', params: params, data: data)
     translations = Array(response_json.first['translations'])
     target_translation = translations.find { |result| result['to'] == to.to_s }
     target_translation['text'] if target_translation
   end
 
-  def translate_array(texts, params = {})
+  def translate_array(texts, params = {}, include_alignment = false)
     raise 'Must provide :to.' if params[:to].nil?
 
-    # Important notice: param order makes sense in SOAP. Do not reorder or delete!
     params = {
-      'from'        => params[:from].to_s,
-      'to'          => params[:to].to_s,
+      'from'             => params[:from].to_s,
+      'to'               => params[:to].to_s
     }
     data = texts.map { |text| { 'Text' => text } }.to_json
-    response_json = api_call('/translate', params, data)
+    response_json = api_client.post('/translate', params: params, data: data)
     response_json.map do |translation|
       target_translation = translation['translations'].find { |result| result['to'] == params['to'].to_s }
       target_translation['text'] if target_translation
@@ -58,7 +137,7 @@ class BingTranslator
       'to'               => params[:to].to_s
     }
     data = texts.map { |text| { 'Text' => text } }.to_json
-    response_json = api_call('/translate', params, data)
+    response_json = api_client.post('/translate', params: params, data: data)
     response_json.map do |translation|
       target_translation = translation['translations'].find { |result| result['to'] == params['to'].to_s }
       [target_translation['text'], target_translation['alignment']['proj']] if target_translation
@@ -68,7 +147,7 @@ class BingTranslator
   def detect(text)
     data = [{ 'Text' => text }].to_json
 
-    response_json = api_call('/detect', {}, data)
+    response_json = api_client.post('/detect', data: data)
     best_detection = response_json.sort_by { |detection| -detection['score'] }.first
     best_detection['language'].to_sym
   end
@@ -78,12 +157,12 @@ class BingTranslator
   end
 
   def supported_language_codes
-    response_json = get_request('/languages', { scope: 'translation' })
+    response_json = api_client.get('/languages', params: { scope: 'translation' }, authorization: false)
     response_json['translation'].keys
   end
 
   def language_names(codes, locale = 'en')
-    response_json = get_request('/languages', { scope: 'translation' }, { 'Accept-Language' => locale})
+    response_json = api_client.get('/languages', params: { scope: 'translation' }, headers: { 'Accept-Language' => locale }, authorization: false)
     codes.map do |code|
       response = response_json['translation'][code.to_s]
       response['name'] unless response.nil?
@@ -92,66 +171,5 @@ class BingTranslator
 
   private
 
-  # Get a new access token and set it internally as @access_token
-  #
-  # @return {hash}
-  # Returns existing @access_token if we don't need a new token yet,
-  # or returns the one just obtained.
-  def get_access_token
-    headers = {
-      'Ocp-Apim-Subscription-Key' => @subscription_key
-    }
-
-    http = Net::HTTP.new(COGNITIVE_ACCESS_TOKEN_URI.host, COGNITIVE_ACCESS_TOKEN_URI.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE if @skip_ssl_verify
-
-    response = http.post(COGNITIVE_ACCESS_TOKEN_URI.path, '', headers)
-    if response.code != '200'
-      raise Exception.new('Invalid credentials')
-    else
-      @access_token = {
-        'access_token' => response.body,
-        'expires_at' => Time.now + 480
-      }
-    end
-  end
-
-  def get_request(path, params, headers = {})
-    encoded_params = URI.encode_www_form(params.merge('api-version' => '3.0'))
-    uri = URI.parse("#{API_HOST}#{path}")
-    uri.query = encoded_params
-    http = Net::HTTP.new(uri.host, 443)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE if @skip_ssl_verify
-    headers = headers.merge('Content-Type' => 'application/json')
-    request = Net::HTTP::Get.new(uri.request_uri, headers)
-
-    JSON.parse(http.request(request).body)
-  end
-
-  def api_call(path, params, data)
-    if @access_token.nil? || @access_token['expires_at'].nil? || 
-      Time.now < @access_token['expires_at']
-      get_access_token
-    end
-
-    encoded_params = URI.encode_www_form(params.merge('api-version' => '3.0'))
-    uri = URI.parse("#{API_HOST}#{path}")
-    uri.query = encoded_params
-    headers = {
-      'Content-Type' => 'application/json',
-      'Authorization' => "Bearer #{@access_token['access_token']}"
-    }
-    http = Net::HTTP.new(uri.host, 443)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE if @skip_ssl_verify
-    request = Net::HTTP::Post.new(uri.request_uri, headers)
-    request.body = data
-
-    response = http.request(request)
-
-    raise Exception.new("Unsuccessful API call: Code: #{response.code}") unless response.code == '200'
-    JSON.parse(response.body)
-  end
+  attr_reader :api_client
 end
